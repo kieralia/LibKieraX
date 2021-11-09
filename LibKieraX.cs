7c0f1493cc1c13fb7d1804d2554964e53af3d5f8
kieralia <tahlia.p.evlyne@protonmail.com>
2021-11-09 16:49:43 +1100

    Comment and organize Source-Code.

--- LibKieraX.cs
+++ LibKieraX.cs
@@ -1,181 +1,299 @@
 
 /*
-*       'LibKieraX'
-*
-*   A collection of simple yet quite usefull data structures.
-*
-*       By Tahlia P. Evlyne ♥♥
-*
+   ♥  'LibKieraX'  ♥
+
+   A collection of simple yet quite usefull data structures.
+
+   Licensed under MIT License.
+
+   *  ♥ By Tahlia P. Evlyne ♥ *
+
 */
 
-
 /* ♥♥ Data structure with 4 points. ♥♥ */
-[ System.Serializable ] public struct Kiera4 {
 
-    public int x , y , z , w ;
+    /* Flagged as Serializable. */
 
-    public Kiera4 ( int x , int y , int z , int w ) {
+[ System.Serializable ] public struct Kiera4 { // ♥
 
-        this.x = x ;
-        this.y = y ;
-        this.z = z ;
-        this.w = w ;
 
-    }
+    /* ♥ Variable declarations. ♥ */
 
-    public override string ToString ( ) {
+        // Integral-Numeric Types for 4 dimensions.
+        public int x , y , z , w ;
 
-        return x + ":" + y + ":" + z + ":" + w ;
 
-    }
+    /* ♥ Constructors ♥ */
 
-    public bool Equals ( Kiera4 other ) {
+        // Main Constructor, for use when assigning values during construction.
+        public Kiera4 ( int x , int y , int z , int w ) { // ♥
 
-        return x == other.x && y == other.y && z == other.z && w == other.w ;
+            // Asigns local numeric values.
+            this.x = x ; this.y = y ; this.z = z ; this.w = w ;
 
-    }
-    public override bool Equals ( object obj ) {
+        } // ♥
 
-        if ( ReferenceEquals ( null , obj ) ) return false ;
-        return obj is Kiera4 other && Equals ( other ) ;
 
-    }
-    public override int GetHashCode ( ) {
+    /* ♥ Override Methods ♥ */
 
-        unchecked {
+        // ToString action override.
+        public override string ToString ( ) { // ♥
 
-            var hashCode = x ;
+            // Returns numeric values combined in string form.
+            return x + ":" + y + ":" + z + ":" + w ;
 
-            hashCode = ( hashCode * 397 ) ^ y ;
-            hashCode = ( hashCode * 397 ) ^ z ;
-            hashCode = ( hashCode * 397 ) ^ w ;
+        } // ♥
 
-            return hashCode ;
 
-        }
+    /* ♥ Equality Members ♥ */
 
-    }
-    public static bool operator == ( Kiera4 left , Kiera4 right ) {
+        // Checks for equality against other Kiera4 instances.
+        public bool Equals ( Kiera4 other ) { // ♥
 
-        return left.Equals ( right ) ;
+            // Return result by checking equity of all 4 dimensions.
+            return x == other.x && y == other.y && z == other.z && w == other.w ;
 
-    }
-    public static bool operator != ( Kiera4 left , Kiera4 right ) {
+        } // ♥
 
-        return ! left.Equals ( right ) ;
+        // Checks for equality against object instances.
+        public override bool Equals ( object obj ) { // ♥
 
-    }
+            // Return result by checking equity via some spooky-oop.
+            if ( ReferenceEquals ( null , obj ) ) return false ;
+            return obj is Kiera4 other && Equals ( other ) ;
 
-}
+        } // ♥
+
+
+    /* ♥ Hashing overrides ♥ */
+
+        // GetHashCode override action.
+        public override int GetHashCode ( ) { // ♥
+
+            // You don't have to check this code. XD
+            unchecked { // ♥
+
+                // Calculate and return the HashCode.
+                var hashCode = x ;
+                hashCode = ( hashCode * 397 ) ^ y ;
+                hashCode = ( hashCode * 397 ) ^ z ;
+                hashCode = ( hashCode * 397 ) ^ w ;
+
+                return hashCode ;
+
+            } // ♥
+
+        } // ♥
+
+
+    /* ♥ Operator declarations. ♥ */
+
+        // Equity-Operator.
+        public static bool operator == ( Kiera4 left , Kiera4 right ) { // ♥
+
+            // Return the result of an Equity-Check.
+            return left.Equals ( right ) ;
+
+        } // ♥
+
+        // Inverse Equity-Operator.
+        public static bool operator != ( Kiera4 left , Kiera4 right ) { // ♥
+
+            // Return the inverse result of an Equity-Check.
+            return ! left.Equals ( right ) ;
+
+        } // ♥
+
+} // ♥
+
 
 /* ♥♥ Data structure with 3 points. ♥♥ */
-[ System.Serializable ] public struct Kiera3 {
 
-    public int x , y , z ;
+    /* Flagged as Serializable. */
 
-    public Kiera3 ( int x , int y , int z ) {
+[ System.Serializable ] public struct Kiera3 { // ♥
 
-        this.x = x ;
-        this.y = y ;
-        this.z = z ;
 
-    }
+    /* ♥ Variable declarations. ♥ */
 
-    public override string ToString ( ) {
+        // Integral-Numeric Types for 3 dimensions.
+        public int x , y , z ;
 
-        return x + ":" + y + ":" + z ;
 
-    }
+    /* ♥ Constructors ♥ */
 
-    public bool Equals ( Kiera3 other ) {
+        // Main Constructor, for use when assigning values during construction.
+        public Kiera3 ( int x , int y , int z ) { // ♥
 
-        return x == other.x && y == other.y && z == other.z ;
+            // Asigns local numeric values.
+            this.x = x ; this.y = y ; this.z = z ;
 
-    }
-    public override bool Equals ( object obj ) {
+        } // ♥
 
-        if ( ReferenceEquals( null , obj ) ) return false ;
-        return obj is Kiera3 other && Equals ( other ) ;
 
-    }
-    public override int GetHashCode ( ) {
+    /* ♥ Override Methods ♥ */
 
-        unchecked {
+        // ToString action override.
+        public override string ToString ( ) { // ♥
 
-            var hashCode = x ;
+            // Returns numeric values combined in string form.
+            return x + ":" + y + ":" + z ;
 
-            hashCode = ( hashCode * 397 ) ^ y ;
-            hashCode = ( hashCode * 397 ) ^ z ;
+        } // ♥
 
-            return hashCode ;
 
-        }
+    /* ♥ Equality Members ♥ */
 
-    }
-    public static bool operator == ( Kiera3 left , Kiera3 right ) {
+        // Checks for equality against other Kiera3 instances.
+        public bool Equals ( Kiera3 other ) { // ♥
 
-        return left.Equals( right ) ;
+            // Return result by checking equity of all 4 dimensions.
+            return x == other.x && y == other.y && z == other.z ;
 
-    }
-    public static bool operator != ( Kiera3 left , Kiera3 right ) {
+        } // ♥
 
-        return ! left.Equals( right ) ;
+        // Checks for equality against object instances.
+        public override bool Equals ( object obj ) { // ♥
 
-    }
+            // Return result by checking equity via some spooky-oop.
+            if ( ReferenceEquals ( null , obj ) ) return false ;
+            return obj is Kiera3 other && Equals ( other ) ;
 
-}
+        } // ♥
+
+
+    /* ♥ Hashing overrides ♥ */
+
+        // GetHashCode override action.
+        public override int GetHashCode ( ) { // ♥
+
+            // You don't have to check this code. XD
+            unchecked { // ♥
+
+                // Calculate and return the HashCode.
+                var hashCode = x ;
+                hashCode = ( hashCode * 397 ) ^ y ;
+                hashCode = ( hashCode * 397 ) ^ z ;
+
+                return hashCode ;
+
+            } // ♥
+
+        } // ♥
+
+
+    /* ♥ Operator declarations. ♥ */
+
+        // Equity-Operator.
+        public static bool operator == ( Kiera3 left , Kiera3 right ) { // ♥
+
+            // Return the result of an Equity-Check.
+            return left.Equals ( right ) ;
+
+        } // ♥
+
+        // Inverse Equity-Operator.
+        public static bool operator != ( Kiera3 left , Kiera3 right ) { // ♥
+
+            // Return the inverse result of an Equity-Check.
+            return ! left.Equals ( right ) ;
+
+        } // ♥
+
+} // ♥
+
 
 /* ♥♥ Data structure with 2 points. ♥♥ */
-[ System.Serializable ] public struct Kiera2 {
 
-    public int x , y ;
+    /* Flagged as Serializable. */
 
-    public Kiera2 ( int x , int y ) {
+[ System.Serializable ] public struct Kiera2 { // ♥
 
-        this.x = x ;
-        this.y = y ;
 
-    }
+    /* ♥ Variable declarations. ♥ */
 
-    public override string ToString ( ) {
+        // Integral-Numeric Types for 2 dimensions.
+        public int x , y ;
 
-        return x + ":" + y ;
 
-    }
+    /* ♥ Constructors ♥ */
 
-    public bool Equals ( Kiera2 other ) {
+        // Main Constructor, for use when assigning values during construction.
+        public Kiera2 ( int x , int y ) { // ♥
 
-        return x == other.x && y == other.y ;
+            // Asigns local numeric values.
+            this.x = x ; this.y = y ;
 
-    }
-    public override bool Equals ( object obj ) {
+        } // ♥
 
-        if ( ReferenceEquals( null , obj ) ) return false ;
-        return obj is Kiera2 other && Equals ( other ) ;
 
-    }
-    public override int GetHashCode ( ) {
+    /* ♥ Override Methods ♥ */
 
-        unchecked {
+        // ToString action override.
+        public override string ToString ( ) { // ♥
 
-            var hashCode = x ;
+            // Returns numeric values combined in string form.
+            return x + ":" + y ;
 
-            hashCode = ( hashCode * 397 ) ^ y ;
+        } // ♥
 
-            return hashCode ;
 
-        }
+    /* ♥ Equality Members ♥ */
 
-    }
-    public static bool operator == ( Kiera2 left , Kiera2 right ) {
+        // Checks for equality against other Kiera2 instances.
+        public bool Equals ( Kiera3 other ) { // ♥
 
-        return left.Equals ( right ) ;
+            // Return result by checking equity of all 4 dimensions.
+            return x == other.x && y == other.y ;
 
-    }
-    public static bool operator != ( Kiera2 left , Kiera2 right ) {
+        } // ♥
 
-        return ! left.Equals ( right ) ;
+        // Checks for equality against object instances.
+        public override bool Equals ( object obj ) { // ♥
 
-    }
+            // Return result by checking equity via some spooky-oop.
+            if ( ReferenceEquals ( null , obj ) ) return false ;
+            return obj is Kiera2 other && Equals ( other ) ;
 
-}
+        } // ♥
+
+
+    /* ♥ Hashing overrides ♥ */
+
+        // GetHashCode override action.
+        public override int GetHashCode ( ) { // ♥
+
+            // You don't have to check this code. XD
+            unchecked { // ♥
+
+                // Calculate and return the HashCode.
+                var hashCode = x ;
+                hashCode = ( hashCode * 397 ) ^ y ;
+
+                return hashCode ;
+
+            } // ♥
+
+        } // ♥
+
+
+    /* ♥ Operator declarations. ♥ */
+
+        // Equity-Operator.
+        public static bool operator == ( Kiera2 left , Kiera2 right ) { // ♥
+
+            // Return the result of an Equity-Check.
+            return left.Equals ( right ) ;
+
+        } // ♥
+
+        // Inverse Equity-Operator.
+        public static bool operator != ( Kiera2 left , Kiera2 right ) { // ♥
+
+            // Return the inverse result of an Equity-Check.
+            return ! left.Equals ( right ) ;
+
+        } // ♥
+
+} // ♥
+
